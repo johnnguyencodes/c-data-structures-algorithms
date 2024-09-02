86958e24ccfbd05613e20e51814e8fe264e6599a
John Nguyen <jnguyen92780@gmail.com>
2024-09-02 15:14:10 -0700

    Added dynamic array functions

--- 03_dynamic_arrays.c
+++ 03_dynamic_arrays.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define INITIAL_SIZE 10
 
@@ -21,7 +22,7 @@ void initialize_array(dynamic_array_t *pA) {
   pA->capacity = INITIAL_SIZE; // Total capacity of the array
 }
 
-// Read array
+// Read entire array
 void read_array(dynamic_array_t *dynamic_array) {
   if (dynamic_array->length == 0) {
     printf("Array is empty\n");
@@ -31,6 +32,20 @@ void read_array(dynamic_array_t *dynamic_array) {
     }
     printf("End of array\n");
   }
+}
+
+// Read element at array index
+int read_element(dynamic_array_t *dynamic_array, int index) {
+  if (dynamic_array->length <= 0) {
+    printf("Unable to remove element since the array is empty.\n");
+    return -1;
+  }
+  if (index >= dynamic_array->length || index < 0) {
+    printf("Index is out of bounds: Index %d\n", index);
+    return -1;
+  } 
+  printf("Reading value at index %d: %d\n", index, dynamic_array->array[index]);
+  return 0;
 }
 
 // Resize the array
@@ -47,17 +62,52 @@ int resize_the_array(dynamic_array_t *dynamic_array) {
     dynamic_array->array = temp;
     dynamic_array->capacity *= 2;
     printf("Array capacity reached its limit and now has been doubled to %d\n", dynamic_array->capacity);
-    return 0;
-  };
+  }
+  return 0;
+}
+
+// Remove element from array
+int remove_element(dynamic_array_t *dynamic_array, int index) {
+  if (dynamic_array->length <= 0) {
+    printf("Unable to remove element since the array is empty.\n");
+    return -1;
+  }
+  if (index >= dynamic_array->length || index < 0) {
+    printf("Index is out of bounds: Index %d\n", index);
+    return -1;
+  } 
+  printf("Removed element %d from index %d\n", dynamic_array->array[index], index);
+  for (int i = index; i < dynamic_array->length; i++) {
+    dynamic_array->array[i] = dynamic_array->array[i + 1];
+  }
+  dynamic_array->length--;
+  return 0;
 }
 
 // Push element to array
 void push_to_array(dynamic_array_t *dynamic_array, int element) {
-  if (resize_the_array(dynamic_array) == 0) {
+  int resize_the_array_result = resize_the_array(dynamic_array);
+  if (resize_the_array_result == 0) {
     dynamic_array->array[dynamic_array->length] = element;
     printf("Added integer %d at position %d\n", element, dynamic_array->length);
     dynamic_array->length++;
+  } else {
+    printf("Exiting push_to_array function, memory reallocation failed\n");
   }
+}
+
+// Pop element from array
+int pop_from_array(dynamic_array_t *dynamic_array) {
+  if (dynamic_array->length <= 0) {
+    printf("Unable to remove element since the array is empty.\n");
+    return -1;
+  }
+  printf("Array length before popping: %d\n", dynamic_array->length);
+  printf("Popped element %d from from end of array\n", dynamic_array->array[dynamic_array->length - 1]);
+  dynamic_array->length--; 
+  printf("Array length is now %d\n", dynamic_array->length);
+
+  return 0;
 }
 
 int main() {
@@ -75,17 +125,37 @@ int main() {
   initialize_array(array); // Initialize the dynamic array
   
   read_array(array);
-
-  push_to_array(array, 10);
+  
+  srand(time(NULL));
+  for (int i = 0; i < 11; i++) {
+    push_to_array(array, rand());
+  };
 
   read_array(array);
+
+  remove_element(array, 3);
+  remove_element(array, 3);
+  remove_element(array, 3);
+  remove_element(array, 3);
   
+  read_array(array);
+
+  pop_from_array(array);
+  pop_from_array(array);
+  pop_from_array(array);
+  pop_from_array(array);
+
+  read_array(array);
+
+  read_element(array, 14);
+
+  read_element(array, 0);
   // Free the allocated memory
   free(array->array);  // Free the integer array inside the structure
   free(array); // Free the structure itself
 
   array = NULL; // Set the pointer to NULL to avoid dangling pointers 
-  
+
   return 0;
 
 }
