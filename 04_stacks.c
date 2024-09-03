d2c45d965582d94628e66ecfb44195cb826b5207
John Nguyen <jnguyen92780@gmail.com>
2024-09-02 20:45:12 -0700

    Implemented stacks data structure

--- 04_stacks.c
+++ 04_stacks.c
@@ -18,18 +18,15 @@ int initialize_stack(stack_t *pS) {
   } 
   pS->length = 0;
   pS->capacity = INITIAL_CAPACITY;
-
   printf("Stack initialized\n");
   return 0;
 }
 
 int check_if_empty_stack(stack_t *pS) {
  if (pS->length <= 0) {
-    printf("Stack is empty\n");
-    return -1;
+    return 1; // Stack is empty
   }
-  printf("Stack has contents\n");
-  return 0;
+  return 0; // Stack is not empty
 }
 
 int check_if_full_stack(stack_t *pS) {
@@ -47,61 +44,48 @@ int check_if_full_stack(stack_t *pS) {
 }
 
 int peek_top_of_stack(stack_t *pS) {
-  int empty_status = check_if_empty_stack(pS);
-  if (empty_status == 0) {
-    printf("Value at top of stack: %d\n", pS->array[pS->length]);
-    return 0;
-  } else {
+  if (check_if_empty_stack(pS)) {
     printf("Stack is empty, cannot peek top of stack\n");
     return -1;
   }
+  int top = pS->array[pS->length - 1];
+  printf("Value at top of stack: %d\n", top);
+  return top;
 }
 
 int push_to_stack(stack_t *pS, int element) {
-  int check_full_stack_result = check_if_full_stack(pS);
-  if (check_full_stack_result == 0) {
-    pS->array[pS->length - 1] = element;
-    printf("Element %d has been pushed to top of stack at index %d\n", element, pS->length);
-    pS->length++;
-    printf("Stack length is now %d\n", pS->length);
-    return 0;
-  } else {
+  if (check_if_full_stack(pS) == -1) {
     printf("Can't push element to top of stack due to memory reallocation error\n");
     return -1;
   }
+  pS->array[pS->length - 1] = element;
+  printf("Element %d has been pushed to top of stack at index %d\n", element, pS->length);
+  pS->length++;
+  printf("Stack length is now %d\n", pS->length);
+  return 0;
 }
 
 int pop_from_stack(stack_t *pS) {
-  int check_if_empty_stack_result = check_if_empty_stack(pS);
-  if (check_if_empty_stack_result == 0) {
-    printf("Element %d has been popped off the stack\n", pS->array[pS->length - 1]);
-    pS->length--;
-    printf("Stack length is now %d\n", pS->length);
-    return 0;
-  } else {
-    printf("Stack is empty, can't pop from stack");
+  if (check_if_empty_stack(pS)) {
+    printf("Stack is empty, can't pop from stack\n");
     return -1;
   }
+  int popped_value = pS->array[pS->length - 1];
+  printf("Element %d has been popped off the stack\n", popped_value);
+  pS->length--;
+  printf("Stack length is now %d\n", pS->length);
 }
 
 int clear_stack(stack_t *pS) {
-  int check_if_empty_stack_result = check_if_empty_stack(pS);
-  if (check_if_empty_stack_result == 0) {
-    for (int i = pS->length; i > 0; i--) {
-      pop_from_stack(pS);
-    }
-    int check_if_empty_stack_result = check_if_empty_stack(pS);
-    if (check_if_empty_stack_result == 0) {
-      printf("Stack is now empty after clearing it");
-      return 0;
-    } else {
-      printf("Error clearing stack");
-      return -1;
-    }
-  } else {
-    printf("Stack is already empty, no need to clear");
+  if (check_if_empty_stack(pS)) {
+    printf("Stack is already empty, no need to clear\n");
     return 0;
+  }    
+  while (pS->length > 0) {
+    pop_from_stack(pS);
   }
+  printf("Stack is now empty after clearing it\n");
+  return 0;
 }
 
 int main() {
@@ -110,31 +94,35 @@ int main() {
   // Allocate memory for stack structure
   stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
   if (stack == NULL) {
-    printf("Memory allocation for struct stack_t failed");
+    printf("Memory allocation for struct stack_t failed\n");
     return -1;
   }
 
   initialize_stack(stack);
 
-  check_if_empty_stack(stack);
-  push_to_stack(stack, incrementor);
-  incrementor++;
+  if (check_if_empty_stack(stack)) printf("Stack is empty\n");
+
+  push_to_stack(stack, incrementor++);
   peek_top_of_stack(stack);
-  push_to_stack(stack, incrementor);
-  incrementor++;
+
+  push_to_stack(stack, incrementor++);
   peek_top_of_stack(stack);
-  push_to_stack(stack, incrementor);
-  incrementor++;
+
+  push_to_stack(stack, incrementor++);
   peek_top_of_stack(stack);
-  check_if_empty_stack(stack);
-  push_to_stack(stack, incrementor);
-  incrementor++;
+
+  if (!check_if_empty_stack(stack)) printf("Stack is has contents\n");
+
+  push_to_stack(stack, incrementor++);
   peek_top_of_stack(stack);
-  push_to_stack(stack, incrementor);
-  incrementor++; 
+
+  push_to_stack(stack, incrementor++);
   peek_top_of_stack(stack);
+
   clear_stack(stack);
-  check_if_empty_stack(stack);
+  
+  if (check_if_empty_stack(stack)) printf("Stack is empty\n");
+
   // Free memory of integer array inside struct 
   free(stack->array);
   // Free memory of the stack struct
